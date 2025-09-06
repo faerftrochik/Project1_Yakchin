1add47a8438d252256862664e8c55a8eb5e5ba7f
faerftrochik <faerftrochik@gmail.com>
2025-09-06 14:29:53 +0300

    2 dubel (Init)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello my litl beibe World!")
+
+    //Задание 1
+
+    println("Введите кол-во двумерного массива строки, столбцы через пробел:")
+
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
