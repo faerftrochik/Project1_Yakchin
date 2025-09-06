ed85d2c2e9949e23f74264581fd185dcc89705a2
faerftrochik <faerftrochik@gmail.com>
2025-09-06 16:45:54 +0300

    2 dubel (Init)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,15 @@ fun main(args: Array<String>) {
 
     //Задание 1
 
-    println("Введите кол-во двумерного массива строки, столбцы через пробел:")
+    println("Введите буквы в строчку:")
+    val testA = readln()
+    println()
+
+    for (i in testA) {
+        print(i)
+    }
+
+
 
 
     // Try adding program arguments via Run/Debug configuration.
